def893ea65568882fd04bd015c097be3302a8f86
NeunG01 <ponanan001@hotmail.com>
2026-01-05 02:30:40 +0700

    update project

--- social/settings.py
+++ social/settings.py
@@ -89,10 +89,17 @@ WSGI_APPLICATION = 'social.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-    )
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv("DB_PASSWORD_YO"),
+        'HOST': 'yamanote.proxy.rlwy.net',
+        'PORT': '41370',
+        
+    }
 }
 
 # Password validation
